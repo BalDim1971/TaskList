21d52df4a38596edfd2520f858b367fd73b68604
BalashovDV <korochun@yandex.ru>
2024-05-24 07:07:53 +0300

    1. Добавлены комментарии для функций. 2. Добавлены описание в README.md.

--- startapp.py
+++ startapp.py
@@ -1,3 +1,9 @@
+############################################################################
+"""
+RESTful API для управления списком задач
+"""
+############################################################################
+
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -5,13 +11,22 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+"""
+Блок настроек для работы с базой данных.
+db_user - пользователь, зарегистрированный в MySQL;
+db_pass - пароль;
+db_name - наименование базы данных;
+db_host - адрес хоста, где запущена MySQL;
+db_port - адрес порта, через который проходит подключение. Необязательный.
+"""
 db_user = 'root'
 db_pass = '12345678'
 db_name = 'tasklist'
 db_host = 'localhost'
+db_port = ':3306'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    f'mysql://{db_user}:{db_pass}@{db_host}:3306/{db_name}'
+    f'mysql://{db_user}:{db_pass}@{db_host}{db_port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -19,6 +34,15 @@ ma = Marshmallow(app)
 
 
 class Tasklist(db.Model):
+    """
+    Модель списка задач.
+    Содержит поля:
+    id - идентификатор задачи, уникальный номер;
+    title - наименование задачи;
+    description - полное описание задачи;
+    created_at - дата и время создания задачи;
+    updated_at - дата и время обновления задачи.
+    """
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500))
@@ -27,6 +51,9 @@ class Tasklist(db.Model):
 
 
 class TaskSchema(ma.SQLAlchemyAutoSchema):
+    """
+    Схема для работы с данными.
+    """
     class Meta:
         model = Tasklist
 
@@ -37,12 +64,24 @@ tasks_schema = TaskSchema(many=True)
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
+    """
+    Получить полный список всех задач.
+    На выходе: JSON-список задач, где каждая задача представляет собой
+    JSON-объект с полями id, title, description, created_at, updated_at.
+    """
     tasks = Tasklist.query.all()
     return jsonify(tasks_schema.dump(tasks))
 
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    """
+    Создать задачу.
+    На входе: JSON-объект с полями title (строка) и
+    description (строка, опционально).
+    На выходе: JSON-объект с полями id, title, description, created_at,
+    updated_at.
+    """
     title = request.json['title']
     description = request.json.get('description', '')
     task = Tasklist(title=title, description=description)
@@ -53,12 +92,26 @@ def create_task():
 
 @app.route('/tasks/<int:id>', methods=['GET'])
 def get_task(id):
+    """
+    Получить данные по задаче.
+    На выходе: JSON-объект с полями id, title, description, created_at,
+    updated_at.
+    """
     task = db.session.get(Tasklist, id)
     return jsonify(task_schema.dump(task))
 
 
 @app.route('/tasks/<int:id>', methods=['PUT'])
 def update_task(id):
+    """
+    Создать задачу.
+    На входе: JSON-объект с полями title (строка, опционально) и
+    description (строка, опционально).
+    На выходе: JSON-объект с полями id, title, description, created_at,
+    updated_at.
+    Поставлена проверка на опциональные параметры. Если пустое значение,
+    данные не изменяются.
+    """
     task = db.session.get(Tasklist, id)
     title = request.json['title']
     description = request.json.get('description', '')
@@ -71,11 +124,19 @@ def update_task(id):
 
 @app.route('/tasks/<int:id>', methods=['DELETE'])
 def delete_task(id):
+    """
+    Удалить задачу.
+    На выходе: Сообщение об успешном удалении.
+    """
     task = db.session.get(Tasklist, id)
+    title = task.title
     db.session.delete(task)
     db.session.commit()
-    return 'Успешно удалено', 204
+    return f'Задача {title} успешно удалена', 204
 
 
+# Основной запуск
 if __name__ == '__main__':
     app.run(debug=True)
+
+############################################################################
